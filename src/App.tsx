fc975c375808330c34ef0cfb7659877ecd601c79
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2022-01-17 23:58:35 +0700

    add async method

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,27 @@ interface AppStates {
   quotes: { quote: string; author: string }[];
 }
 
+// const getData = async () => {
+//   const url =
+//     'https://raw.githubusercontent.com/ccrsxx/random-quote-machine/main/asets/quotes.json';
+//   let data = null;
+
+//   try {
+//     data = await fetch(url);
+//     if (data.status !== 200) {
+//       throw data.status;
+//     }
+//   } catch (e) {
+//     console.log('ERROR', e);
+//   }
+
+//   return data?.json();
+// };
+
+// getData()
+//   .then((data) => console.log(data))
+//   .catch((e) => console.log(e));
+
 class RandomQuoteMachine extends React.Component<AppProps, AppStates> {
   constructor(props: AppProps) {
     super(props);
@@ -15,9 +36,20 @@ class RandomQuoteMachine extends React.Component<AppProps, AppStates> {
     };
   }
 
-  componentDidMount() {}
+  async componentDidMount() {
+    await fetch(
+      'https://raw.githubusercontent.com/ccrsxx/random-quote-machine/main/assets/quotes.json'
+    )
+      .then((raw) => raw.json())
+      .then(({ quotes }) => {
+        this.setState({
+          quotes
+        });
+      });
+  }
 
   render() {
+    console.log(this.state.quotes);
     return <h1>Hello World</h1>;
   }
 }
